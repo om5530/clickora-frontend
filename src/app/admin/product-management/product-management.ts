1d02710c0ee91458b43887bb2cb3c2c85cd0b617
omk5530 <omkarbacha25@gmail.com>
2025-07-06 18:39:14 +0530

    feat: enable standalone component for product management

--- src/app/admin/product-management/product-management.ts
+++ src/app/admin/product-management/product-management.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-management',
+  standalone: true,
   imports: [],
   templateUrl: './product-management.html',
   styleUrl: './product-management.scss'
